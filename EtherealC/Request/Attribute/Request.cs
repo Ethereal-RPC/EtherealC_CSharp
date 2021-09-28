df4cdee11f50011dc8538c0752ad3006bd008afd
Jianxian <30934916+m839336369@users.noreply.github.com>
2021-09-28 20:38:22 +0800

    Request增设请求类型，使用Flags，灵活配置本地方法与远程方法

--- EtherealC/Request/Attribute/Request.cs
+++ EtherealC/Request/Attribute/Request.cs
@@ -5,8 +5,26 @@ namespace EtherealC.Request.Attribute
     [AttributeUsage(AttributeTargets.Method)]
     public class Request : System.Attribute
     {
+        [Flags]
+        public enum InvokeTypeFlags
+        {
+            Local = 0x1,
+            Remote = 0x2,
+            Fail = 0x4,
+            Success = 0x8,
+            ReturnLocal = 0x10,
+            ReturnRemote = 0x20,
+            Timeout = 0x40,
+            All = 0x80,
+        }
+
+        private InvokeTypeFlags invokeType = InvokeTypeFlags.Remote | InvokeTypeFlags.ReturnRemote;
+
         private string[] paramters = null;
         private int timeout = -1;
+
+
+        public InvokeTypeFlags InvokeType { get => invokeType; set => invokeType = value; }
         public string[] Paramters { get => paramters; set => paramters = value; }
         public int Timeout { get => timeout; set => timeout = value; }
     }
--- EtherealC/Request/WebSocket/WebSocketRequest.cs
+++ EtherealC/Request/WebSocket/WebSocketRequest.cs
@@ -27,7 +27,9 @@ namespace EtherealC.Request.WebSocket
         protected override object Invoke(MethodInfo targetMethod, object[] args)
         {
             Attribute.Request rpcAttribute = targetMethod.GetCustomAttribute<Attribute.Request>();
-            if (rpcAttribute != null)
+            object localResult = null;
+            object remoteResult = null;
+            if ((rpcAttribute.InvokeType & Attribute.Request.InvokeTypeFlags.Local) == 0)
             {
                 //这里要连接字符串，发现StringBuilder效率高一些.
                 StringBuilder methodid = new StringBuilder(targetMethod.Name);
@@ -72,7 +74,6 @@ namespace EtherealC.Request.WebSocket
                 if (targetMethod.ReturnType == typeof(void))
                 {
                     client.SendClientRequestModel(request);
-                    return null;
                 }
                 else
                 {
@@ -92,23 +93,49 @@ namespace EtherealC.Request.WebSocket
                         {
                             if (result.Error != null)
                             {
-                                throw new TrackException(TrackException.ErrorCode.Runtime, $"ErrorCode:{result.Error.Code} Message:{result.Error.Message} Data:{result.Error.Data}");
+                                if ((rpcAttribute.InvokeType & Attribute.Request.InvokeTypeFlags.Fail) != 0)
+                                {
+                                    localResult = targetMethod.Invoke(this, args);
+                                }
+                                else throw new TrackException(TrackException.ErrorCode.Runtime, $"ErrorCode:{result.Error.Code} Message:{result.Error.Message} Data:{result.Error.Data}");
                             }
                             else if (Config.Types.TypesByName.TryGetValue(result.ResultType, out AbstractType type))
                             {
-                                return type.Deserialize((string)result.Result);
+                                remoteResult = type.Deserialize((string)result.Result);
+                                if ((rpcAttribute.InvokeType & Attribute.Request.InvokeTypeFlags.Success) != 0
+                                    || (rpcAttribute.InvokeType & Attribute.Request.InvokeTypeFlags.All) != 0
+                                    )
+                                {
+                                    localResult = targetMethod.Invoke(this, args);
+                                }
                             }
                             else throw new TrackException(TrackException.ErrorCode.Runtime, $"C#中的{result.ResultType}类型转换器尚未注册");
+                        }
+                        else if((rpcAttribute.InvokeType & Attribute.Request.InvokeTypeFlags.Timeout) != 0)
+                        { 
+                            localResult = targetMethod.Invoke(this, args);
                         }
                     }
                     finally
                     {
                         tasks.TryRemove(id,out ClientRequestModel value);
                     }
-                    return null;
                 }
             }
-            return null;
+            else
+            {
+                localResult = targetMethod.Invoke(this, args);
+            }
+
+            if ((rpcAttribute.InvokeType & Attribute.Request.InvokeTypeFlags.ReturnRemote) != 0)
+            {
+                return remoteResult;
+            }   
+            if((rpcAttribute.InvokeType & Attribute.Request.InvokeTypeFlags.ReturnLocal) != 0)
+            {
+                return localResult;
+            }
+            return remoteResult;
         }
         #endregion
     }
